492ad147721b6e8682a481845da3dc91119fb273
micker <goodluck.was@gmail.com>
2020-03-20 21:05:22 +0800

    update bg chain

---
+++ BGChain/BGChain/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BGChain
+//
+//  Created by Micker on 2020/3/20.
+//  Copyright © 2020 Micker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
